37b7ed9ee1ec04e1eca162d4bcc743b8fdd3bb9e
Victor <victorsantosflx@gmail.com>
2025-08-20 20:20:44 -0300

    DSFDSFSDF

--- site/tests/calc.test.js
+++ site/tests/calc.test.js
@@ -1,21 +1,21 @@
 const calcular = require("../src/calc.js");
 
 test("soma de 2 + 3 deve ser 5", () => {
-  expect(calcular(2, 3, "soma")).toBe(5);
+    expect(calcular(2, 3, "soma")).toBe(5);
 });
 
-test("subtração de 10 - 4 deve ser 6", () => {
-  expect(calcular(10, 4, "subtracao")).toBe(6);
+test("subtracão de 10 - 4 deve ser 6", () => {
+    expect(calcular(10, 4, "subtracao")).toBe(6);
 });
 
 test("multiplicação de 3 * 7 deve ser 21", () => {
-  expect(calcular(3, 7, "multiplicacao")).toBe(21);
+    expect(calcular(3, 7, "multiplicacao")).toBe(21);
 });
 
 test("divisão de 20 / 4 deve ser 5", () => {
-  expect(calcular(20, 4, "divisao")).toBe(5);
+    expect(calcular(20, 4, "divisao")).toBe(5);
 });
 
 test("divisão por zero deve lançar erro", () => {
-  expect(() => calcular(10, 0, "divisao")).toThrow("Divisão por zero");
+    expect(() => calcular(10, 0, "divisao")).toThrow("Divisão por zero");
 });
